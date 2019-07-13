c1aab21d8571f3f71d52d7a492373828cfe8711d
Goare Mao <mygoare@gmail.com>
2019-07-13 18:30:25 +0800

    fix bug: change bundle.js publicPath to relative,  minify html

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,18 +1,26 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
     entry: './app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/',
+        publicPath: './',
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: 'index.html',
         }),
     ],
+    optimization: {
+        minimizer: [
+          new TerserJSPlugin({}), 
+          new OptimizeCSSAssetsPlugin({})
+        ]
+    },
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
