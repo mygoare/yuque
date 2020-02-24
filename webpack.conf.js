dfe899e85d06f554e38d424adb39ffeef0773df1
mygoare <mygoare@gmail.com>
2020-02-24 17:15:00 +0800

    优化：去除缓存

--- webpack.conf.js
+++ webpack.conf.js
@@ -2,16 +2,19 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+        filename: '[name].[contenthash].js',
         publicPath: './',
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
+            title: 'UED设计小报 - 每周四更新',
             template: 'index.html',
         }),
     ],
