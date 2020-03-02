fcc6d32a72d4d8e7f7abac7a87f6963c93d67450
Goare Mao <goare.mao@rentalist.co.uk>
2020-03-02 19:37:07 +0800

    fix

--- .travis/push.sh
+++ .travis/push.sh
@@ -6,14 +6,15 @@ setup_git() {
 }
 
 commit_files() {
+  git checkout master
   git status
   git add data/*.json
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 push_commits() {
-  git remote add travis-build https://${GH_TOKEN}@github.com/mygoare/yuque.git > /dev/null 2>&1
-  git push travis-build master
+  git remote add travis-build https://${GITHUB_TOKEN}@github.com/mygoare/yuque.git > /dev/null 2>&1
+  git push travis-build master --quiet
 }
 
 setup_git
