1b0a14532139a22d8365fd0ee1fada8a9b027411
Goare Mao <goare.mao@rentalist.co.uk>
2020-03-02 19:30:48 +0800

    fix

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
+  git config --global user.email "travis@travis-ci.com"
   git config --global user.name "Travis CI"
 }
 
 commit_files() {
+  git status
   git add data/*.json
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 push_commits() {
-  git push --quiet origin master
+  git remote add travis-build https://${GH_TOKEN}@github.com/mygoare/yuque.git > /dev/null 2>&1
+  git push travis-build master
 }
 
 setup_git
